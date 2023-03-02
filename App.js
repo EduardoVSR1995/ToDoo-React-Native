481d1368af6819b593874f4c641d45a181deef0a
EduardoVSR1995 <eduardvitor7@gmail.com>
2023-03-02 14:03:27 -0300

    learning react-native

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Header } from 'react-native/Libraries/NewAppScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Header>ToDooozinha</Header>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
